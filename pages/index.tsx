602f08d77c78772f6573b27fd0c7a122e95dc9de
Dmytro Pryshliak <dmytro.pryshliak@dxc.com>
2022-02-04 17:25:17 +0200

    Replace style approach to index page. Edit content of index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { StyledHomePage, StyledHomePageContent } from './index.styles'
 import HomeLink from '../components/HomeLink/HomeLink'
 import Title from '../components/Title/Title'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.main}>
+    <StyledHomePage>
       <Head>
         <title>Dmytro Pryshliak</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className='container'>
+      <StyledHomePageContent>
         <Title text="I'm Dmytro Pryshliak." />
-        <HomeLink text='Front End Developer @ Luxoft.' url='/dev'/>
-        <HomeLink text='With passion for UX.' url='/des'/>
-      </div>
-    </div>
+        <HomeLink text='Front End Developer @ Luxoft.' url='/dev' />
+      </StyledHomePageContent>
+    </StyledHomePage>
   )
 }
 
